ee441a52648910927ac9963edbc61af86ee8b32b
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-06-12 14:01:43 +0100

    render App.js component with FriendList component

---
+++ friends/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import FriendList  from './components/Friends/FriendList';
+
+function App() {
+  return (
+    <div className="App">
+      <FriendList />
+    </div>
+  );
+}
+
+export default App;
